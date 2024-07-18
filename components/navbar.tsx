782b7c3e58df7df7f6792bedd51573ca0db3bc28
Chamod Rashmika <89768971+chamodRash@users.noreply.github.com>
2024-07-18 23:14:49 +0530

    navbar reset

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,11 +1,6 @@
 import Image from "next/image";
-import logo from 
 
 type Props = {};
 export const Navbar = ({}: Props) => {
-  return (
-    <div>
-      <Image />
-    </div>
-  );
+  return <div>navbar</div>;
 };
