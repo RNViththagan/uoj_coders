37529d5c8e10b23647c3a08079709dca05ca2f4c
Chamod Rashmika <89768971+chamodRash@users.noreply.github.com>
2024-07-24 09:15:13 +0530

    award section finished

--- components/awards-section.tsx
+++ components/awards-section.tsx
@@ -3,12 +3,12 @@ import Spline from "@splinetool/react-spline/next";
 type Props = {};
 export const AwardsSection = ({}: Props) => {
   return (
-    <div className="bg-neutral-950 w-full pb-10 pt-32 relative overflow-hidden">
-      <h1 className=" font-varino text-4xl text-center font-bold text-white uppercase tracking-widest">
+    <div className="bg-neutral-950 w-full pt-32 relative overflow-hidden">
+      <h1 className="font-varino text-4xl text-center font-bold text-white uppercase tracking-widest">
         Awards
       </h1>
       <Spline
-        className="-mt-32 scale-110"
+        className=""
         scene="https://prod.spline.design/m-drsuXxnwnv-bkL/scene.splinecode"
       />
     </div>
