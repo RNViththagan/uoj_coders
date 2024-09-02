3685f112cb209e5645eb6166345cf1562647885e
Chamod Rashmika <89768971+chamodRash@users.noreply.github.com>
2024-09-03 01:06:51 +0530

    timeline design updated

--- components/timeline-section.tsx
+++ components/timeline-section.tsx
@@ -1,11 +1,6 @@
 "use client";
 
 import React from "react";
-import Image from "next/image";
-import { twMerge } from "tailwind-merge";
-import { TracingBeam } from "@/components/ui/tracing-beam";
-
-import { timelineSteps } from "@/data/timeline";
 import {
   UserPen,
   UserRoundX,
@@ -13,6 +8,7 @@ import {
   Rocket,
   Trophy,
 } from "lucide-react";
+import { Timeline } from "./ui/timeline";
 
 export function TimelineSection() {
   return (
@@ -20,44 +16,103 @@ export function TimelineSection() {
       <h1 className="font-varino text-xl lg:text-4xl text-center font-bold text-white mb-16 uppercase tracking-widest">
         Timeline
       </h1>
-      <TracingBeam className="px-6 bg-neutral-950">
-        <div className="pt-16 grid grid-rows-5 gap-12 max-w-3/4 mx-auto antialiased relative">
-          {timelineSteps.map((item, index) => (
-            <div
-              key={`content-${index}`}
-              className={`w-full h-full row-start-${index + 1} flex ${
-                index % 2 !== 0 ? `justify-end` : ``
-              }`}>
-              <div
-                className={`w-96 py-5 flex items-center gap-x-10 ${
-                  index % 2 === 0 ? `flex-row-reverse` : `flex-row`
-                }`}>
-                <div className="bg-gradient-to-bl from-[#18CCFC] via-[#6344F5] to-[#AE48FF] rounded-full p-4 text-neutral-300">
-                  {item.badge === "UserPen" ? (
-                    <UserPen size={30} />
-                  ) : item.badge === "UserRoundX" ? (
-                    <UserRoundX size={30} />
-                  ) : item.badge === "UserRoundMinus" ? (
-                    <UserRoundMinus size={30} />
-                  ) : item.badge === "Rocket" ? (
-                    <Rocket size={30} />
-                  ) : item.badge === "Trophy" ? (
-                    <Trophy size={30} />
-                  ) : null}
-                </div>
-                <div className="flex flex-col gap-y-1 items-start justify-center">
-                  <h2 className="text-white text-lg lg:text-xl font-semibold w-fit tracking-wider">
-                    {item.title}
-                  </h2>
-                  <p className={"text-base lg:text-lg text-slate-200"}>
-                    {item.description}
-                  </p>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
-      </TracingBeam>
+      <div className="container lg:w-3/4 mx-auto">
+        <Timeline data={timelineSteps} />
+      </div>
     </div>
   );
 }
+
+const timelineSteps = [
+  {
+    title: "August 12, 2024",
+    content: (
+      <div className="w-96 py-5 flex items-center gap-x-10 flex-row">
+        <div className="bg-white rounded-full p-4 text-neutral-900">
+          <UserPen size={30} />
+        </div>
+        <div className="flex flex-col gap-y-1 items-start justify-center">
+          <h2 className="text-white text-lg lg:text-xl font-semibold w-fit tracking-wider">
+            Registration Opens
+          </h2>
+          <p className="text-base lg:text-lg text-slate-200">
+            Registers opening for all participants.
+          </p>
+        </div>
+      </div>
+    ),
+  },
+  {
+    title: "August 28, 2024",
+    content: (
+      <div className="w-96 py-5 flex items-center gap-x-10 flex-row">
+        <div className="bg-white rounded-full p-4 text-neutral-900">
+          <UserRoundX size={30} />
+        </div>
+        <div className="flex flex-col gap-y-1 items-start justify-center">
+          <h2 className="text-white text-lg lg:text-xl font-semibold w-fit tracking-wider">
+            Registration Closes
+          </h2>
+          <p className="text-base lg:text-lg text-slate-200">
+            Last day to register for the competition.
+          </p>
+        </div>
+      </div>
+    ),
+  },
+  {
+    title: "September 1, 2024",
+    content: (
+      <div className="w-96 py-5 flex items-center gap-x-10 flex-row">
+        <div className="bg-white rounded-full p-4 text-neutral-900">
+          <UserRoundMinus size={30} />
+        </div>
+        <div className="flex flex-col gap-y-1 items-start justify-center">
+          <h2 className="text-white text-lg lg:text-xl font-semibold w-fit tracking-wider">
+            Elimination Round
+          </h2>
+          <p className="text-base lg:text-lg text-slate-200">
+            This round will be conducted online.
+          </p>
+        </div>
+      </div>
+    ),
+  },
+  {
+    title: "September 14, 2024",
+    content: (
+      <div className="w-96 py-5 flex items-center gap-x-10 flex-row">
+        <div className="bg-white rounded-full p-4 text-neutral-900">
+          <Rocket size={30} />
+        </div>
+        <div className="flex flex-col gap-y-1 items-start justify-center">
+          <h2 className="text-white text-lg lg:text-xl font-semibold w-fit tracking-wider">
+            Final Round
+          </h2>
+          <p className="text-base lg:text-lg text-slate-200">
+            Participants are expected to be present at the venue of the
+            competition.
+          </p>
+        </div>
+      </div>
+    ),
+  },
+  {
+    title: "September 15, 2024",
+    content: (
+      <div className="w-96 py-5 flex items-center gap-x-10 flex-row">
+        <div className="bg-white rounded-full p-4 text-neutral-900">
+          <Trophy size={30} />
+        </div>
+        <div className="flex flex-col gap-y-1 items-start justify-center">
+          <h2 className="text-white text-lg lg:text-xl font-semibold w-fit tracking-wider">
+            Award Ceremony
+          </h2>
+          <p className="text-base lg:text-lg text-slate-200">
+            Celebrate the winners and participants.
+          </p>
+        </div>
+      </div>
+    ),
+  },
+];
